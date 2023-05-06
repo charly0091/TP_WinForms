3cc7e03517ce84915ebf7472561db8da2f5c02a7
Maria Clara Visconti <claravisconti@hotmail.com>
2023-05-06 17:12:02 -0300

    correcciones

--- WindowsFormsApp/frmAltaCategoria.Designer.cs
+++ WindowsFormsApp/frmAltaCategoria.Designer.cs
@@ -39,18 +39,20 @@
             // lblId
             // 
             this.lblId.AutoSize = true;
-            this.lblId.Location = new System.Drawing.Point(88, 36);
+            this.lblId.Location = new System.Drawing.Point(90, 108);
+            this.lblId.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblId.Name = "lblId";
-            this.lblId.Size = new System.Drawing.Size(21, 16);
+            this.lblId.Size = new System.Drawing.Size(19, 13);
             this.lblId.TabIndex = 0;
             this.lblId.Text = "Id:";
             // 
             // lblDescripcion
             // 
             this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.Location = new System.Drawing.Point(27, 68);
+            this.lblDescripcion.Location = new System.Drawing.Point(44, 134);
+            this.lblDescripcion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(82, 16);
+            this.lblDescripcion.Size = new System.Drawing.Size(66, 13);
             this.lblDescripcion.TabIndex = 1;
             this.lblDescripcion.Text = "Descripcion:";
             // 
@@ -59,18 +61,19 @@
             this.txtDescripcion.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtDescripcion.Location = new System.Drawing.Point(125, 65);
+            this.txtDescripcion.Location = new System.Drawing.Point(118, 132);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtDescripcion.Name = "txtDescripcion";
-            this.txtDescripcion.Size = new System.Drawing.Size(100, 22);
+            this.txtDescripcion.Size = new System.Drawing.Size(149, 20);
             this.txtDescripcion.TabIndex = 2;
-
             // 
             // btnAceptar
             // 
             this.btnAceptar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnAceptar.Location = new System.Drawing.Point(30, 128);
+            this.btnAceptar.Location = new System.Drawing.Point(46, 180);
+            this.btnAceptar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAceptar.Name = "btnAceptar";
-            this.btnAceptar.Size = new System.Drawing.Size(84, 29);
+            this.btnAceptar.Size = new System.Drawing.Size(63, 24);
             this.btnAceptar.TabIndex = 4;
             this.btnAceptar.Text = "Aceptar";
             this.btnAceptar.UseVisualStyleBackColor = true;
@@ -79,9 +82,10 @@
             // btnCancelar
             // 
             this.btnCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCancelar.Location = new System.Drawing.Point(150, 128);
+            this.btnCancelar.Location = new System.Drawing.Point(209, 180);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(84, 29);
+            this.btnCancelar.Size = new System.Drawing.Size(63, 24);
             this.btnCancelar.TabIndex = 5;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
@@ -91,17 +95,19 @@
             // 
             this.lblAutoId.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lblAutoId.AutoSize = true;
-            this.lblAutoId.Location = new System.Drawing.Point(134, 36);
+            this.lblAutoId.Location = new System.Drawing.Point(197, 108);
+            this.lblAutoId.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAutoId.Name = "lblAutoId";
-            this.lblAutoId.Size = new System.Drawing.Size(59, 16);
+            this.lblAutoId.Size = new System.Drawing.Size(48, 13);
             this.lblAutoId.TabIndex = 6;
             this.lblAutoId.Text = "lblAutoId";
             // 
             // frmAltaCategoria
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(348, 169);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(334, 311);
             this.Controls.Add(this.lblAutoId);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnAceptar);
@@ -109,6 +115,7 @@
             this.Controls.Add(this.lblDescripcion);
             this.Controls.Add(this.lblId);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmAltaCategoria";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Nueva Categoria";
--- WindowsFormsApp/frmAltaMarca.Designer.cs
+++ WindowsFormsApp/frmAltaMarca.Designer.cs
@@ -39,42 +39,47 @@
             // lblId
             // 
             this.lblId.AutoSize = true;
-            this.lblId.Location = new System.Drawing.Point(85, 42);
+            this.lblId.Location = new System.Drawing.Point(122, 105);
+            this.lblId.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblId.Name = "lblId";
-            this.lblId.Size = new System.Drawing.Size(21, 16);
+            this.lblId.Size = new System.Drawing.Size(19, 13);
             this.lblId.TabIndex = 0;
             this.lblId.Text = "Id:";
             // 
             // lblDescripcion
             // 
             this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.Location = new System.Drawing.Point(24, 80);
+            this.lblDescripcion.Location = new System.Drawing.Point(76, 136);
+            this.lblDescripcion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(82, 16);
+            this.lblDescripcion.Size = new System.Drawing.Size(66, 13);
             this.lblDescripcion.TabIndex = 1;
             this.lblDescripcion.Text = "Descripcion:";
             // 
             // lblAutoId
             // 
             this.lblAutoId.AutoSize = true;
-            this.lblAutoId.Location = new System.Drawing.Point(133, 42);
+            this.lblAutoId.Location = new System.Drawing.Point(158, 105);
+            this.lblAutoId.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAutoId.Name = "lblAutoId";
-            this.lblAutoId.Size = new System.Drawing.Size(59, 16);
+            this.lblAutoId.Size = new System.Drawing.Size(48, 13);
             this.lblAutoId.TabIndex = 2;
             this.lblAutoId.Text = "lblAutoId";
             // 
             // txtDescripcion
             // 
-            this.txtDescripcion.Location = new System.Drawing.Point(123, 77);
+            this.txtDescripcion.Location = new System.Drawing.Point(150, 134);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtDescripcion.Name = "txtDescripcion";
-            this.txtDescripcion.Size = new System.Drawing.Size(100, 22);
+            this.txtDescripcion.Size = new System.Drawing.Size(76, 20);
             this.txtDescripcion.TabIndex = 3;
             // 
             // btnAceptar
             // 
-            this.btnAceptar.Location = new System.Drawing.Point(27, 128);
+            this.btnAceptar.Location = new System.Drawing.Point(78, 175);
+            this.btnAceptar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAceptar.Name = "btnAceptar";
-            this.btnAceptar.Size = new System.Drawing.Size(82, 29);
+            this.btnAceptar.Size = new System.Drawing.Size(62, 24);
             this.btnAceptar.TabIndex = 4;
             this.btnAceptar.Text = "Aceptar";
             this.btnAceptar.UseVisualStyleBackColor = true;
@@ -82,9 +87,10 @@
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(150, 128);
+            this.btnCancelar.Location = new System.Drawing.Point(170, 175);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(82, 29);
+            this.btnCancelar.Size = new System.Drawing.Size(62, 24);
             this.btnCancelar.TabIndex = 5;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
@@ -92,9 +98,10 @@
             // 
             // frmAltaMarca
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(348, 169);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(334, 311);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.txtDescripcion);
@@ -102,6 +109,7 @@
             this.Controls.Add(this.lblDescripcion);
             this.Controls.Add(this.lblId);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmAltaMarca";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Nueva Marca";
